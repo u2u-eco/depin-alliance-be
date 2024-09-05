63ca4e2c664893d2552433ad765db7f2c6e656eb
holden.fcode <hungch@timebird.org>
2024-09-05 09:23:53 +0700

    edit

--- src/main/java/xyz/telegram/depinalliance/common/models/response/MissionPartnerResponse.java
+++ src/main/java/xyz/telegram/depinalliance/common/models/response/MissionPartnerResponse.java
@@ -1,11 +1,9 @@
 package xyz.telegram.depinalliance.common.models.response;
 
-import java.util.List;
-
 /**
  * @author holden on 03-Sep-2024
  */
-public class MissionResponse {
+public class MissionPartnerResponse {
   public String type;
 //  public List<Object>
 }
