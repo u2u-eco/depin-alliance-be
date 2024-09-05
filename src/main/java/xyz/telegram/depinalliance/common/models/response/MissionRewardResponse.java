3b66055498ff989465976887447af1419f5f1e83
holden.fcode <hungch@timebird.org>
2024-09-05 09:21:51 +0700

    edit mission res

---
+++ src/main/java/xyz/telegram/depinalliance/common/models/response/MissionRewardResponse.java
@@ -0,0 +1,9 @@
+package xyz.telegram.depinalliance.common.models.response;
+
+/**
+ * @author holden on 03-Sep-2024
+ */
+public class MissionRewardResponse {
+  public String type;
+//  public List<Object>
+}
