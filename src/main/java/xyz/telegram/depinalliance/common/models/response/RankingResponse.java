16e1b072433aa4b95eeb60ca404876de24d93e21
holden.fcode <hungch@timebird.org>
2024-09-06 09:11:18 +0700

    edit rank

--- src/main/java/xyz/telegram/depinalliance/common/models/response/RankingResponse.java
+++ src/main/java/xyz/telegram/depinalliance/common/models/response/RankingResponse.java
@@ -10,11 +10,11 @@ import java.math.BigDecimal;
 public class RankingResponse {
   public String username;
   public String avatar;
-  public BigDecimal miningPowerReal;
+  public BigDecimal miningPower;
 
   public RankingResponse(String username, String avatar, BigDecimal miningPowerReal) {
     this.username = username;
     this.avatar = avatar;
-    this.miningPowerReal = Utils.stripDecimalZeros(miningPowerReal);
+    this.miningPower = Utils.stripDecimalZeros(miningPowerReal);
   }
 }
