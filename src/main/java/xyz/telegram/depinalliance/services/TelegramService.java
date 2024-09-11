fef2bb8462fff1e980a586e23656442c4a199341
holden.fcode <hungch@timebird.org>
2024-09-11 16:30:55 +0700

    edit verify join channel tele

--- src/main/java/xyz/telegram/depinalliance/services/TelegramService.java
+++ src/main/java/xyz/telegram/depinalliance/services/TelegramService.java
@@ -91,10 +91,13 @@ public class TelegramService {
   }
 
   public boolean verifyJoinChannel(String chatId, String userId) {
-    TelegramResponse response = telegramClient.getChatMember(botTokenVerify, chatId, userId);
-    if (response.ok && "member".equals(response.result.status) || "administrator".equals(
-      response.result.status) || "creator".equals(response.result.status)) {
-      return true;
+    try {
+      TelegramResponse response = telegramClient.getChatMember(botTokenVerify, chatId, userId);
+      if (response.ok && "member".equals(response.result.status) || "administrator".equals(
+        response.result.status) || "creator".equals(response.result.status)) {
+        return true;
+      }
+    } catch (Exception e) {
     }
     return false;
   }
