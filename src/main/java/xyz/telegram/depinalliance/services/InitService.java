dfd5a130ad1550cdfecee00b7d89ebe7e0423490
holden.fcode <hungch@timebird.org>
2024-09-21 08:57:59 +0700

    add partner miniton

--- src/main/java/xyz/telegram/depinalliance/services/InitService.java
+++ src/main/java/xyz/telegram/depinalliance/services/InitService.java
@@ -1270,9 +1270,9 @@ public class InitService {
 
         mission = new Mission();
         mission.groupMission = partner.name;
-        mission.name = "Play game to get 5000 FREE USDT CHIPS";
+        mission.name = "Play (non- $points) game to get 5000 FREE USDT CHIPS";
         mission.type = Enums.MissionType.PLAY_MINI_TON;
-        mission.isFake = true;
+        mission.isFake = false;
         mission.description = "Play game to get 5000 FREE USDT CHIPS";
         mission.orders = 4;
         mission.url = " https://t.me/MiniTonBot/MiniTonGame?startapp=from_U2U";
